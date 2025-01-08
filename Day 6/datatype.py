3c8c9b9d29eb1afa1848ec15e4b9331dbde8bd1f
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-08 12:38:57 +0600

    Add files via upload
    I have done Data type in my Day 6

---
+++ Day 6/datatype.py
@@ -0,0 +1,13 @@
+kodu = "hello how are you?"
+print(kodu)
+Tanisha_djl = "what are you doing?"
+print(Tanisha_djl)
+hafsa1 = "happy happy happy"
+print(hafsa1)
+hafsa_1 = "you are grate"
+print(hafsa_1)
+
+
+
+
+
