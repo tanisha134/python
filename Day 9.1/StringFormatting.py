8464e2211d8536a6e5c0baca6dcacb272e535b77
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-11 17:44:34 +0600

    Add files via upload
    I have learn StringFormatting in python today

---
+++ Day 9.1/StringFormatting.py
@@ -0,0 +1,11 @@
+num1 = 20
+num2 = 10
+print(f"this is my super number{50 + 50}", num1 + num2 )
+username = "tanisha"
+Roll = "39"
+print(f"my name is {username} & my class roll is {Roll}")
+
+
+
+
+
