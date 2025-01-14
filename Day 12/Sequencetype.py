f74dc6d2c56857313555a0dfea605590e3d51973
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-14 12:32:55 +0600

    Add files via upload
    i have learn there Sequence type data

---
+++ Day 12/Sequencetype.py
@@ -0,0 +1,19 @@
+#list type data
+li = ['Tanisha','Hafsa','Asmita','Yousra']
+li[0] = "Happy"
+print(li)
+print(type(li))
+#tuple type data
+tup = (5,10,15,20,25)
+
+print(type(tup))
+
+#range type data
+ran = range(6)
+
+print(ran)
+
+for i in ran:
+    print(i)
+
+
