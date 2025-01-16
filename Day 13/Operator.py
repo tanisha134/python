b3ddd560a203544bf1524de4a1b65e6e7ccf39c0
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-16 21:04:36 +0600

    Add files via upload
    I have learn there Operator

---
+++ Day 13/Operator.py
@@ -0,0 +1,28 @@
+#Addition
+a = 10
+b = 20
+print(a + b)
+#Subtraction
+print(b-a)
+
+#Multiplication
+print(a*b)
+
+#Division
+print(b/a)
+
+#Modulus %
+print(b%a)
+
+#Exponentiation **
+x = 5
+y = 3
+print(x**y)#5*5*5
+
+#Floor Division//
+sum1 = 15
+sum2 = 2
+print(sum1 // sum2)
+
+
+
