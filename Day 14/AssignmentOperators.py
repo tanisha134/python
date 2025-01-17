92f2692ab7ada53a374630e9eaec7cab7fdb9beb
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-17 22:55:59 +0600

    Add files via upload
    I have learn there assinment operators in python

---
+++ Day 14/AssignmentOperators.py
@@ -0,0 +1,40 @@
+#Assignment Operators
+a = 10
+a +=50
+print(a)
+
+a -= 40
+print(a)
+#Comparison operators
+x = 10
+y = 20
+print(x==y)
+
+x = 30
+y = 40
+print(x!=y)
+
+x = 10
+y = 50
+print(x>y)
+
+x = 60
+y = 50
+print(x<y)
+
+x = 100
+y = 200
+print(x>=y)
+
+x = 20
+y = 10
+print(x<=y)
+
+
+
+
+
+
+
+
+
