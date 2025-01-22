2254e0be89c1096067f9b4b8eebd0c0ad25cebd8
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-22 20:47:47 +0600

    Add files via upload
    I have learn there how to made python list

---
+++ Day 18/Pythonlist.py
@@ -0,0 +1,30 @@
+#int
+li = [1,2,3,4,5]
+print(type(li))
+print(li)
+li[1] = 10
+print(li)
+
+#string
+lis = ["Tanisha","Hafsa","Afrin"]
+print(type(lis))
+print(lis)
+#boolean
+list = [True,True,False,False]
+print(type(list))
+print(list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
