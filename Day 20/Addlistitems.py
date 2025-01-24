282852485e921166f7d2a266704bf97700c1c04c
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-24 21:56:06 +0600

    Add files via upload
    I have learn there how to Add listitems

---
+++ Day 20/Addlistitems.py
@@ -0,0 +1,14 @@
+Tanisha = ["Channle name","website","Group","page","errtty"]
+print(Tanisha[3])
+#change list item
+Tanisha[4] = "Linkedin page"
+print(Tanisha)
+#append
+Tanisha.append(10)
+print(Tanisha)
+#insert
+Tanisha.insert(1,"Google +")
+print(Tanisha)
+
+
+
