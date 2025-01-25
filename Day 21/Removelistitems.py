589ea97abf3bf01ff0e60e35d526acb15097de8f
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-25 23:13:57 +0600

    Add files via upload
    I have learn there How to remove list items in Day 21

---
+++ Day 21/Removelistitems.py
@@ -0,0 +1,25 @@
+#The remove() method removes the specified itemst
+Newlist = ["Tanisha","Hafsa","Hablu",420]
+#Newlist.remove("Hafsa")
+#print(Newlist)
+#Newlist.remove(420)
+#print(Newlist)
+
+#The pop() method method removes the specified index
+#Newlist.pop(2)
+#print(Newlist)
+
+#The del keyword also removes the specified index:
+#del Newlist[1]
+#print(Newlist)
+
+#Clear the list
+print(Newlist)
+Newlist.clear()
+print(Newlist)
+
+
+
+
+
+
