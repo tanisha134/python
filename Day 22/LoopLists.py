73334ad259def4ea89a48189e4cb5bdadd9ef077
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-26 22:16:18 +0600

    Add files via upload
    I have learn there how to run loop list

---
+++ Day 22/LoopLists.py
@@ -0,0 +1,25 @@
+#You can loop through the list items by using a for loop:
+LoopList = ["Tanisha","Hafsa","Soha","Shafa","Asmita"]
+for friends in LoopList:
+     print(friends)
+#Use the range() and len() functions to create a suitable iterable
+for T in range(len(LoopList)):
+     print(T)
+
+#print all item, using a while loop to go through all the index numbers
+
+y = 0
+
+while y <len(LoopList):
+     print(LoopList[y])
+     y = y + 1
+
+
+
+
+
+
+
+
+
+
