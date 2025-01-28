a60e2b9ace9eea44e17cead3a7048f4968a802af
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-28 22:46:55 +0600

    Add files via upload
    I have learn there Sort list

--- Day 24/Sortlist.py
+++ Day 24/Sortlist.py
@@ -1,1 +1,14 @@
 # Short list
+Number = [3,6,9,1,2,13]
+eng = ["b","d","c","a","e"]
+eng.sort()
+Number.sort()
+print(eng)
+print(Number)
+# Reverse list
+num = [1,2,3,4,5,6,7,8,9]
+num.sort(reverse = True )
+print(num)
+eng = ["a","b","c","d","e","f"]
+eng.sort(reverse = True)
+print(eng)
