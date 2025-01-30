17a3f786913395e34f281ae182d753e233bf46f5
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-30 18:32:44 +0600

    Add files via upload
    I have learn there how to join to list

---
+++ Day 26/Jointwolists.py
@@ -0,0 +1,14 @@
+# Python - Join list
+num1 = [1,2,3]
+num2 = [4,5,6]
+num3 = num1 + num2
+print(num3)
+
+print(num1)
+num1.extend(num2)
+print(num1)
+#string
+Eng1 = ["A","B","C"]
+Eng2 = ["D","E","F"]
+Eng1.extend(Eng2)
+print(Eng1)
