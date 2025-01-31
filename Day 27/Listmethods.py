7c6854dbf9765fa1bda84a6b71b997b35f6c58e5
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-01-31 22:08:54 +0600

    Add files via upload
    I have write there list methods

---
+++ Day 27/Listmethods.py
@@ -0,0 +1,21 @@
+1.#Append() Adds an element at the end of the list
+
+2.#Clear() Removes all the elements from the list
+
+3.#Copy() Returns a copy of the list
+
+4.#Count() Retuens the number of elements with the specified value
+
+5.#Extend() Add the elements of a list (or any iterable),to the end of the current list
+
+6.#Index() Returns the index of the first element with the specified value
+
+7.#Insert() Adds an element at the specified position
+
+8.#Pop() Removes the element at the specified position
+
+9.#Remove() Removes the item with the specified value
+
+10.#Reverse() Reverses the order of the list
+
+11.#Sort() Sorts the list
