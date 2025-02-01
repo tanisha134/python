82e06391cc696c7fe75adfacc1e7175d375501b5
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-01 22:48:27 +0600

    Add files via upload
    I have learn in Day 28 is Python list exercises

---
+++ Day 28/PythonlistExercises.py
@@ -0,0 +1,15 @@
+# print the second item in the fruits list
+fruits = ["Apple","Banana","Cherry"]
+print(fruits[1])
+
+# Change the value from "apple" to "kiwi",in the fruits list.
+fruits = ["Apple","Banana","Cherry"]
+print(fruits)
+fruits[0] = "kiwi"
+print(fruits)
+
+# Use the append method to add "orange" to the fruits list
+
+fruits = ["Apple","Banana","Cherry"]
+fruits.append("orange")
+print(fruits)
