6e8719da90fbc991449a5549adab54c11460060c
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-03 20:48:47 +0600

    Add files via upload
    I have learn there Python Tuples

---
+++ Day 30/PythonTuples.py
@@ -0,0 +1,16 @@
+#Tuple type
+NewTuples = (1,2,3,"Tanisha",False,"Hafsa" )
+print(type(NewTuples))
+#NewTuples[2] = 20
+#print(NewTuples)
+
+# Negative Index
+NewTuples[-2]
+print(NewTuples[-1])
+
+#Range of Indexes
+NewTuples[2:4]
+print(NewTuples[0:5])
+
+
+
