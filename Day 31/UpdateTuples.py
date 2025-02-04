a13657a3995ec01b53428c90a399d63b87e3f039
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-04 21:31:53 +0600

    Add files via upload
    I have learn there How to update Tuples

---
+++ Day 31/UpdateTuples.py
@@ -0,0 +1,11 @@
+# python - Update Tuples
+ThisTuple = ("Tanisha","Hafa","Soha","Shafa")
+
+a = list(ThisTuple)
+print(type(list(ThisTuple)))
+
+a.append("Sharodia")
+print(a)
+
+ThisTuple = tuple(a)
+print(ThisTuple)
