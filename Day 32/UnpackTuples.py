af99525aba55f88ca05a2e9134853a5c3bc7e28d
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-05 22:04:45 +0600

    Add files via upload
    I have learn there how to unpack Tuples

---
+++ Day 32/UnpackTuples.py
@@ -0,0 +1,14 @@
+#Unpack Tuples in normal way
+Fruits = ("Apple","Banana","Cherry","Bluebarry","Orange")
+(a,b,c,d,e) = Fruits
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+#Unpack Tuples with use of star sign
+Fruits = ("Apple","Banana","Cherry","Bluebarry","Orange")
+(*a,b) = Fruits
+print(a)
+
+
