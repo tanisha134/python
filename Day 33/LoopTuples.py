4ca8eeea6ebb52ffb555cd8a9e83c0d48d23733d
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-06 23:15:15 +0600

    Add files via upload
    I have learn there Loop tuples

---
+++ Day 33/LoopTuples.py
@@ -0,0 +1,14 @@
+#Tuple loop
+Fruits = ["TAnisha","Hafsa","Soha","Shafa","Sharooo"]
+for h in Fruits:
+    print(h)
+#Range and len
+for t in range(len(Fruits)):
+    print(Fruits[t])
+
+Hashi = ("TAnisha","Hafsa","Soha","Shafa","Sharooo")
+p = 0
+while p <len(Hashi):
+    print(Hashi[p])
+    p = p + 1
+
