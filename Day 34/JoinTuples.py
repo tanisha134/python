6a16e46becfa934ca5ef5adb3ba25d9b07f21911
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-07 21:51:58 +0600

    Add files via upload
    I have learn there How to join two tuples

---
+++ Day 34/JoinTuples.py
@@ -0,0 +1,14 @@
+# join to Tuples
+num1 = (1,2,3,4,5,6)
+num2 = (7,8,9,10,11,12)
+Num3 = num1 + num2
+print(Num3)
+
+# Multiply Tuple
+Num1 = (1,2,3,4,5,6,7,8,9,10)
+print(Num1 * 2)
+Num4 = ("Tanisha","Hafsa","Shafa","Soha","sharoo")
+print(Num4 * 3)
+
+
+
