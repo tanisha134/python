b600fe0cda2902befe117e03ca50f242ff7fd165
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-08 22:59:06 +0600

    Add files via upload
    I have learn there Tuple method

---
+++ Day 35/TupleMethod.py
@@ -0,0 +1,11 @@
+#Tuple Method
+#indexing method
+Ha = ("happy","Sad","Angry","Sleppy","lovely",100)
+indexing = Ha.index("lovely")
+indexing2 = Ha.index(100)
+print(indexing)
+print(indexing2)
+#Count method
+Ha = ("happy","Sad","Angry","Sleppy","lovely",100,"happy","Happy",100)
+Counting = Ha .count("happy")
+print(Counting)
