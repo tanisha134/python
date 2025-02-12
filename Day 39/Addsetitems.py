30936f3f7d1710ba82e38b462a359ff503907567
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-12 22:21:16 +0600

    Add files via upload
    I have learn there how to add set item

---
+++ Day 39/Addsetitems.py
@@ -0,0 +1,19 @@
+#Add set item
+myset1 = {"Tanisha","Hafsa","Soha","Shafa"}
+
+myset1.add("Sahroo")
+
+print(myset1)
+
+#Set update ()
+myset2 = {"Tanisha","Hafsa","Soha","Shafa"}
+
+list1  = [1,2,3]
+
+print(type(list1))
+
+myset2.update(list1)
+
+print(myset2)
+
+
