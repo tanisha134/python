92d5e73f516800083f0c00541ffa02e05b8d66c9
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-13 22:59:21 +0600

    Add files via upload
    I have learn there How to remove set items

---
+++ Day 40/RemoveSetItems.py
@@ -0,0 +1,30 @@
+#Remove set items
+myset1 = {1,2,3,4,5,6}
+
+print(myset1)
+myset1.remove(1)
+
+print(myset1)
+
+#discard method
+myset1.discard(10)
+
+print(myset1)
+Myset2 = {"Tanisha","Hafsa","soha"}
+
+Myset2.discard("soha")
+
+print(Myset2)
+#Pop method in set
+Myset3 = {"Tanisha","Hafsa","soha"}
+
+Myset3.pop()
+
+print(Myset3)
+
+#Clear method
+Myset4 = {"Tanisha","Hafsa","soha"}
+
+Myset4.clear()
+
+print(Myset4)
