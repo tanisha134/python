bd79c1673d195c9515e11122160437d9b4486213
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-15 23:17:13 +0600

    Add files via upload
    I have learn there How to join sets

---
+++ Day 42/JoinSets.py
@@ -0,0 +1,11 @@
+#The union()method returns a new set with all items from both sets:
+Set1 = {"A","B","C"}
+Set2 = {1,2,3}
+Set3 = Set1.union(Set2)
+print(Set3)
+
+#Update method
+Set4 = {"A","B","C","D"}
+Set5 = {1,2,3,4}
+Set5.update(Set4)
+print(Set5)
