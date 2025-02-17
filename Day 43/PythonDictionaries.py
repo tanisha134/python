860fae469d3358522395bf31f7c6d3a30ad9624b
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-17 22:35:08 +0600

    Add files via upload
    I have learn there Python Dictionaries

---
+++ Day 43/PythonDictionaries.py
@@ -0,0 +1,22 @@
+#Dictionaries
+StudentInfo = {
+  "Shafa":{
+        "name":"Shafa",
+        "location": "",
+        "Study":"7",
+        "roll":"39",
+        "number":2134567653
+    }
+}
+studentInfo = {
+    "Soha":{
+        "name":"Soha",
+        "Location": "Khulna",
+        "Study":"1",
+        "Roll":"20",
+        "Number":213456765334
+    }
+}
+
+print(studentInfo["Soha"])
+print(studentInfo["Soha"]["name"])
