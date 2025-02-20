8e66d2cb8f56cc448ccd113f8e8979058bea037b
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-20 23:12:49 +0600

    Add files via upload
    I have learn there How to copy Dictionary in python

---
+++ Day 47.2/PythonCopyDictionary.py
@@ -0,0 +1,16 @@
+#Copy Dictionarys
+StudentInfo = {
+    "Sum1":20,
+     "Sum2":40,
+     "Sum3":60
+
+}
+print(StudentInfo)
+
+myDict = StudentInfo.copy()
+print(myDict)
+
+print(StudentInfo)
+
+r = dict(StudentInfo)
+print(r)
