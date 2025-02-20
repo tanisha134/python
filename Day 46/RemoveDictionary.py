c67189f63713e21848cbc0ee4d888a706ddf825b
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-20 23:10:04 +0600

    Add files via upload
    I have learn there how to Remove items from Dictionary

---
+++ Day 46/RemoveDictionary.py
@@ -0,0 +1,34 @@
+
+StudentInfo = {
+  "Shafa":{
+        "name":"Shafa",
+        "location": "",
+        "Study":"7",
+        "roll":"39",
+        "number":2134567653
+    }
+}
+studentInfo = {
+    "Soha":{
+        "name":"Soha",
+        "Location": "Khulna",
+        "Study":"1",
+        "Roll":"20",
+        "Number":213456765334
+    },
+    'year':1981
+}
+studentInfo['year'] = 2012
+#print(studentInfo)
+
+#studentInfo.update({"Soha":"Soha read in class 7"})
+#print(studentInfo["Soha"])
+
+studentInfo.pop("Soha")
+studentInfo.pop("year")
+print(studentInfo)
+print(studentInfo)
+
+#Pop item
+StudentInfo.popitem()
+print(StudentInfo)
