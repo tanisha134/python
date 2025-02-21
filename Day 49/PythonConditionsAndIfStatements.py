010b1dcf6331d2cdff59b9f8ec000914154b7b21
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-21 22:00:53 +0600

    Add files via upload
    I have learn there How to apply Condition and if Statement in python

---
+++ Day 49/PythonConditionsAndIfStatements.py
@@ -0,0 +1,36 @@
+#Python Conditions And If Statements
+
+#Equals: a == b
+
+#Not Equal: a != b
+
+#Less than: a < b
+
+#Less than or equal to: a <= b
+
+#Greater than: a > b
+
+#Greater than or equal to: a >= b
+
+#Condition
+a = 100
+b = 80
+
+if a > b:
+    print("ami b er theke boro")
+
+elif a > b:
+    print("ami b er theke boro")
+
+elif a < b:
+    print(" b a er theke boro")
+
+elif a != b:
+    print("Kono ta e thik noy")
+
+elif a == b:
+    print("2 ta e shoman")
+
+
+else :
+    print("a b er theke boro na")
