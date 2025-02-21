fa927f30d8c90cfb7879df5a752960b3a7137f34
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-21 22:01:42 +0600

    Add files via upload
    I have learn there Python loops

---
+++ Day 50/PythonLoops.py
@@ -0,0 +1,13 @@
+#While loop
+tanisha = 0
+
+while tanisha <101:
+    print("yes tanisha is less than",tanisha)
+    tanisha = tanisha + 1
+
+#for loop
+fruits = ["apple","banana","cherry"]
+for b in fruits:
+    if b == "banana":
+        break
+    print(b)
