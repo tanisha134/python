7437c382d91ff4a07793da2c128163021ae772a7
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-22 22:36:45 +0600

    Add files via upload
    I have learn there Python Functions

---
+++ Day 51/PythonFunctions.py
@@ -0,0 +1,28 @@
+Sum1 = 20
+Sum2 = 30
+Sum3 = Sum1 +Sum2
+print(Sum3)
+
+Sum4 = 20
+Sum5 = 40
+Sum6 = Sum5 - Sum4
+print(Sum6)
+
+#Functions
+
+
+def TanishaFunc(a,b):
+    Sum = a + b
+    print(Sum)
+
+TanishaFunc(40,56)
+TanishaFunc(50,35)
+TanishaFunc(89,49)
+
+
+def Minus (x,y):
+    Sum = x - y
+    print(Sum)
+
+Minus(100,50)
+Minus(10000000000000000000,20000000000000000)
