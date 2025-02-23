bcf9ba8e16733b47956b131604e3bc988a056770
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-23 23:10:51 +0600

    Add files via upload
    I have learn there How to use break & continue function in python

---
+++ Day 52/PythonBreakandContinue.py
@@ -0,0 +1,12 @@
+#break keyword
+Tanishalist = ["Eshan","Hablu","Gablu","tutul","Putul","Tetul","tultul",8,9,10]
+for x in range(len(Tanishalist)):
+    if x == 5:
+        break
+    print(x)
+
+#Continue keyword
+for r in range(len(Tanishalist)):
+    if r == 5:
+        continue
+    print(r)
