dd1d85f005f3079e2120e1e8f530b39a47816f29
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-25 23:22:01 +0600

    Add files via upload
    I have learn there Classes And Objects in python

---
+++ Day 58/PythonClassesAndObjects.py
@@ -0,0 +1,22 @@
+#Class
+class Tanisha:
+    Name = ""
+    Number = ""
+    roll = ""
+
+#Object
+a = Tanisha()
+b = Tanisha()
+c = Tanisha()
+d = Tanisha()
+
+a.Name = "Hafsa"
+a.Number = 345
+b.Name = "Soha"
+c.Number = 123
+d.roll = "1"
+print(a.Name)
+print(b.Name)
+print(c.Number)
+print(a.Number)
+print(d.roll)
