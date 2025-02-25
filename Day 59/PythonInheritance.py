57c33c36a880b804b24178b4efd92219603d9ec2
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-25 23:23:05 +0600

    Add files via upload
    I have learn there Inheritance in python today

---
+++ Day 59/PythonInheritance.py
@@ -0,0 +1,18 @@
+#Inheritance
+
+class baba:
+    car = "BMW"
+    tk  = "100 Crore"
+    home = "10 Floor"
+
+class kaka(baba):
+
+    BrokenPhone = ""
+    BrokenHome  =  ""
+
+y = kaka()
+print(y.car)
+
+
+
+
