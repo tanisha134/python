d5f4bceea7ebc3103901069aa7de62e4c7ef133a
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-02-28 22:50:14 +0600

    Add files via upload
    I have learn there Try And Except in python today

---
+++ Day 68/Python Try And Except.py
@@ -0,0 +1,16 @@
+#Try and except
+try:
+    print("give me output number 1 line")
+except:
+    print(x)
+
+print("give me function")
+
+print("give me 500 lines code")
+print("give me 500 lines code")
+print("give me 500 lines code")
+print("give me 500 lines code")
+
+
+
+
