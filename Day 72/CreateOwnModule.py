792d0e144ad669257bdf0d77481a378f870ed4a1
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-03-01 23:01:39 +0600

    Add files via upload
    I have learn there how to make my own Module in python

---
+++ Day 72/CreateOwnModule.py
@@ -0,0 +1,15 @@
+def tanisha():
+    print("Tanisha is now An Module")
+
+def hafsa():
+    print("hello world")
+
+def kaki():
+    print("Ami kaki")
+
+Person1 = {
+    "name": "John",
+    "age":36,
+    "country":"Norway"
+}
+list = ["Jannatul","Kazi","Hira mama"]
---
+++ Day 72/ImportingMyOwnModule.py
@@ -0,0 +1,17 @@
+import CreateOwnModule
+
+import CreateOwnModule as Module
+
+Module.tanisha()
+
+Module.hafsa()
+
+Module.kaki()
+
+print(Module.Person1["name"])
+
+print(Module.list)
+
+from CreateOwnModule import tanisha
+
+tanisha()
