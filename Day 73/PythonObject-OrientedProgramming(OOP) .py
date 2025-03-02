57d7975556c56075cf196ab967100be3450f6400
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-03-02 23:55:37 +0600

    Add files via upload
    I have learn there Object-Oriented Programming (OOP) in python today

---
+++ Day 73/PythonObject-OrientedProgramming(OOP) .py
@@ -0,0 +1,30 @@
+#INTRODUTION TO OOP
+
+#1. Class & Object
+
+#2. Inheritance
+
+#3. Abstraction
+
+#4. Encapsulation
+
+#5. Polymorphism
+
+#A class is a code template for creating objects.
+# Objects have member variables and have behaviour associated with them.
+# In python a class is created by the keyword class .
+# An object is created using the constructor of the class.
+# This object will then be called the instance of the class.
+
+#Inheritance allows you to build new classes by reusing and extending the functionality of existing ones.
+
+#Abstraction is  a programming concept that hides complex implementation details while exposing only essential information and functionalities to users.
+
+#Encapsulation is a fundamental object-oriented principle in Python.
+# It protects your classes from accidental changes or deletions and promotes code reusability and maintainability.
+
+#Polymorphism is a term used to refer to an object's ability to take on multiple forms.
+
+
+
+
