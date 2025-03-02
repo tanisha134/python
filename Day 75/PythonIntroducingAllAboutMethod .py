deaf4a3f9ad042b4e329e6bb6d86c42271fa2a75
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-03-02 23:57:11 +0600

    Add files via upload
    I have learn there Constructor in python today

---
+++ Day 75/PythonIntroducingAllAboutMethod .py
@@ -0,0 +1,11 @@
+
+class parentInfo1:
+    def __init__(self ,name, number):
+        print(f"my name is {name} , & My Number is {number} ")
+
+        @classmethod
+        def MyName(LMS) :
+            print("hello python")
+
+p1 = parentInfo1("hafsa","012345565456")
+p1.MyName()
