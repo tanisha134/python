51727ab165a2d581d3249b9e89604ed34d312f50
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-03-03 22:01:20 +0600

    Add files via upload
    i have learn there Abstraction (OOP) in python today

---
+++ Day 76/PythonAbstraction(OOP).py
@@ -0,0 +1,4 @@
+#Abstraction is a programming concept that hides complex implementation details while exposing only essential information and functionalities to users
+#It allows us to hide the complexity of systems and focus only on the essential details.
+# In Python, abstraction is implemented through abstract classes and methods, which make our code cleaner, more manageable, and easier to maintain.
+# By using abstraction, we can divide the complexity of a program into manageable parts.
