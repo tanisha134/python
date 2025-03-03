1a2d1ed5bebcacb39a1e55177e449bf818e61979
I am Tanisha , and highly interested in Coding <jannatulnurtanisha63@gmail.com>
2025-03-03 21:59:48 +0600

    Add files via upload
    I have learn there Introducing All About Method in python today

--- Day 75/PythonIntroducingAllAboutMethod .py
+++ Day 75/PythonIntroducingAllAboutMethod .py
@@ -1,11 +1,42 @@
+class ParentInfo:
+    def __init__(self, name, number):
+        print(f"my name is {name}, & My Number Is {number}")
 
-class parentInfo1:
-    def __init__(self ,name, number):
-        print(f"my name is {name} , & My Number is {number} ")
+    def YourName(self , Number):
+        self.Number = Number
 
-        @classmethod
-        def MyName(LMS) :
-            print("hello python")
+        print(Number)
 
-p1 = parentInfo1("hafsa","012345565456")
+    @classmethod
+    def MyName(LMS):
+        print("hello python")
+
+p1 = ParentInfo("Tanisha" , "01234456787")
+ParentInfo.MyName()
 p1.MyName()
+
+p1.YourName("hafsa")
+
+#Instance Method
+class ClassName:
+    def InstanceMethod(self):
+        print("hello Instance Method")
+
+    @classmethod
+    def ClassMethod(cls):
+        print("This Is Class Method")
+
+    @staticmethod
+    def staticMethod():
+        print("This Is StaticMethod")
+
+
+
+v1 = ClassName()
+v1.InstanceMethod()
+
+v1.ClassMethod()
+ClassName.ClassMethod()
+
+v1.staticMethod()
+ClassName.staticMethod()
